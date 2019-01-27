2a5a858dec953403d1a04e17b7a33be973ed7a41
baskaryabase <baymax1298@gmail.com>
2019-01-27 15:30:38 +0530

    first commit

---
+++ precache-manifest.a3eef6252a0c7518c5fee3bea10e15d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "de046282f11856ca383c44c1423d136a",
+    "url": "/static/media/Image2.de046282.jpg"
+  },
+  {
+    "revision": "9d850bc2716c3fba0e9b410ddbfa24cc",
+    "url": "/static/media/Image1.9d850bc2.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd08ddb1c9d5fc8032da",
+    "url": "/static/js/main.dd08ddb1.chunk.js"
+  },
+  {
+    "revision": "a1deffea244c8405e4fa",
+    "url": "/static/js/1.a1deffea.chunk.js"
+  },
+  {
+    "revision": "dd08ddb1c9d5fc8032da",
+    "url": "/static/css/main.61713a51.chunk.css"
+  },
+  {
+    "revision": "051eacbf85ac1e48e4e6f4e58edd32c8",
+    "url": "/index.html"
+  }
+];
