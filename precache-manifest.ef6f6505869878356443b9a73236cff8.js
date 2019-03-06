f9dd733e1b4b283a5f4beae3a096deeb55bc29a8
johnJD47 <johndisilwa47@gmail.com>
2019-03-06 09:46:34 +0530

    added upgrad

---
+++ precache-manifest.ef6f6505869878356443b9a73236cff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "78233028d6b9a8354e2e",
+    "url": "/static/js/main.78233028.chunk.js"
+  },
+  {
+    "revision": "c43d701e8673288e2d99",
+    "url": "/static/js/2.c43d701e.chunk.js"
+  },
+  {
+    "revision": "78233028d6b9a8354e2e",
+    "url": "/static/css/main.8f2f019e.chunk.css"
+  },
+  {
+    "revision": "6baa2d2ed2451d2041bf940aa9437b98",
+    "url": "/index.html"
+  }
+];
