d95915a32b3d5c71e4beaa8cb8174caeb1c91195
baskaryabase <baymax1298@gmail.com>
2019-03-09 23:25:13 +0530

    added dataweave

---
+++ dataweave/precache-manifest.a4bcc88c46efa98cff53b0aedee7c743.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "bad622ef85fc100ef6b7",
+    "url": "/static/js/main.7a8e093b.chunk.js"
+  },
+  {
+    "revision": "5080a232ff0b04eb67bb",
+    "url": "/static/js/2.70a409f6.chunk.js"
+  },
+  {
+    "revision": "d42113d8ff7de4a36a4a14faacc5c41d",
+    "url": "/index.html"
+  }
+];
