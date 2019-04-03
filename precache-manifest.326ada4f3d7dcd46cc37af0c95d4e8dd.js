26aeca5414c2d5a4bd8f23ca9be02e590978e2d4
Baymax <baymax1298@gmail.com>
2019-04-03 09:52:30 +0530

    added my resume

---
+++ precache-manifest.326ada4f3d7dcd46cc37af0c95d4e8dd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "785cbd35a3a624b93e75",
+    "url": "/static/js/main.417c0dc7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b323830bcb83ff624757cb5c4f44eb58",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04d8d9bb462e46ec35456acc9cb1da64.js"
+  "/precache-manifest.326ada4f3d7dcd46cc37af0c95d4e8dd.js"
 );
 
 workbox.clientsClaim();
