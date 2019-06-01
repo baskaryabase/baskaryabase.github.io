d9e2d1edb386d12f255bff372b01b160d1a1b7f3
Baymax <baymax1298@gmail.com>
2019-06-01 12:18:46 +0530

    added biblr

---
+++ bible/precache-manifest.b729c89e77a94c4f9c22a56a6f269f44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9657358ba44634dd840505f0a04949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6505b23dc8aa26c959ed",
+    "url": "/static/css/main.0c9869f8.chunk.css"
+  },
+  {
+    "revision": "5b3b80bf7973068c4bcf",
+    "url": "/static/js/2.1d44a4ba.chunk.js"
+  },
+  {
+    "revision": "6505b23dc8aa26c959ed",
+    "url": "/static/js/main.e9cc0fac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
